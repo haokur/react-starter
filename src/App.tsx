11c0a5662f335a85bc9afb125001cad53be4ae0d
AllIsWell <admin@haokur.com>
2024-06-13 10:57:50 +0800

    修改路由方式

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import AppRoutes from "./routes/routes"
 
