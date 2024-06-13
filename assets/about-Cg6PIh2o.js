d52466521bd2cd7648458a0b02a03bafe9c66470
haokur <haokur@users.noreply.github.com>
2024-06-13 02:37:09 +0000

    deploy: 7e91b7796a9518c60de81bb82d1c211fe582c3a3

---
+++ assets/about-Cg6PIh2o.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CjtNFTF5.js";const s=function(){return t.jsx("div",{children:"this is AboutPage"})};export{s as default};
---
+++ assets/contact-DEvxdotJ.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CjtNFTF5.js";const o=function(){return t.jsx("div",{children:"this is ContactPage"})};export{o as default};
