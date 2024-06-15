31d4839460f9a9a55fbdf3b3749050d75bed5915
haokur <haokur@users.noreply.github.com>
2024-06-15 04:22:16 +0000

    deploy: 764e3e4064eeee585ccdfda92ff967eb67efe146

--- assets/contact-BacMV-VF.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BSRqGD4f.js";const o=function(){return t.jsx("div",{children:"this is ContactPage"})};export{o as default};
---
+++ assets/contact-DWVF1PcI.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CI85H7W2.js";const o=function(){return t.jsx("div",{children:"this is ContactPage"})};export{o as default};
