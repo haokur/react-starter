a5591b2a1588058b5b49a1f0c4d3872e49c91f9c
haokur <haokur@users.noreply.github.com>
2024-06-16 13:44:07 +0000

    deploy: 4e43c0565fe6ce0653a2f09ae55b0b13561c0cee

--- assets/contact-Dqkz20d_.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Di6D4kmV.js";const o=function(){return t.jsx("div",{children:"this is ContactPage"})};export{o as default};
---
+++ assets/contact-UO44o6sy.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CLtm76_4.js";const o=function(){return t.jsx("div",{children:"this is ContactPage"})};export{o as default};
